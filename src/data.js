5697ad3223eb53b74e2b197d45284bf39dece784
AlejandraAAO <aventurero_7_20@hotmail.com>
2018-07-09 16:29:08 -0500

    reestructuramiento de la logica y el ui

--- src/data.js
+++ src/data.js
@@ -1,40 +1,103 @@
-/*aqui fetch
+window.computeUsersStats =(users, progress,courses)=>{
+ try{  
+    //console.log(users, progress,courses);
+    
+     let students = users.filter(objUser => objUser.role ==="student");
+     const calculatePercent = (progressUser, courses) => {
+        let count = 0;
+        courses.forEach(nameCourse =>{            
+          
+          //console.log(progressUser);
+            if(progressUser.hasOwnProperty(nameCourse)){
+                count += progressUser[nameCourse].percent;
+            };
+        });
+        return (count/courses.length);
+    };
 
-1) computeUsersStats(users, progress, courses)
-2) sortUsers(users, orderBy, orderDirection)
-3) filterUsers(users, search)
-4) processCohortData(options)
 
-const processCohortData = (options) => {
-    let users = options.cohortData.users;
-    let progress = options.cohortData.progress
-    let usersWithGrades = computeUsersStates(users,progress,courses);
-}
+    const calculateStats = (progressUser, courses, type) =>{
+        let complete = 0;
+        let total = 0;
+        let scoreSum =0;//quizzes
+        let scoreAvg = 0;//quizzez
+        courses.forEach((nameCourse)=>{
+            
+            
+            if(progressUser.hasOwnProperty(nameCourse)){
+                const units = Object.values(progressUser[nameCourse].units);
+                  //console.log(units);
+                units.forEach((objUnit)=>{
+                    const parts = Object.values(objUnit.parts);
+                   // console.log(parts);//array
+                    switch(type){
+                        case "practice":
+                        const exercises = parts.filter(objPart => objPart.type === "practice" && objPart.hasOwnProperty("exercise"));
+                        console.log(exercises);
+                        exercises.forEach((objExercise)=>{
+                            exercisesTotal += exercises.length;
+                            exercisesCompleted += objExercise.completed;
+                        })
+                        break;
+                        case "read":
+                        const reads = parts.filter(objPart => objPart.type === "read");
+                        const readsCompleted = parts.filter(objPart => objPart.type === "read" && objPart.completed === 1);
+                        console.log(reads, readsCompleted)
+                        break;
+                        case "quiz":
+                        break;
+                    }
+                    
 
-const computeUserStates = (users, progress, courses)=>{
-    let ststs ={percent:0,exercises:{total:0,}}
-    stats.percent =100
-    console.log(stats.percent)
-    stats.exercises,total = 25
-    console.log("computeUserStates");
-}
+                })
+            };
 
-const sortUsrs =(users,orderby,orderdirection){
-    console.log(sortusers)
-}
-como llamar a processCohortData
- const userwithstats = processcohortdata(options);
- console.log(userwithstats)
- userwithstats.forEach(function() {
+        })
+        let response=  {
+            total: total,
+            completed: complete,
+            percent: complete/total
+        };
+        if(type ==="quiz"){
+            respuesta.scoreSum =scoreSum;
+            respuesta.scoreAvg =scoreAvg
+        }
+    }
 
- })
-*/
-//primeera funcion
-window.computeUsersStats = (users, progress, courses) => {
 
+
+    students = students.map(objStudent =>{
+       const objectWithPropertysOfUsers = {
+           name : objStudent.name,
+            stats:{
+                percent : calculatePercent(progress[objStudent.id], courses),
+                exercises : calculateStats(progress[objStudent.id], courses, "practice"),
+                reads :null,
+                quizzes: null,
+                
+            }
+        }
+        return objectWithPropertysOfUsers;
+    })
+    console.log(students);  
+    return students;
+ }catch (error) {
+   console.log(error.name);
+   console.log(error.message);
+   console.log("Error linea: ", error.lineNumber);        
+   return;    
+ }  
     
 }
 
 
-
-
+//4ta funcion
+window.processCohortData =(options)=>{
+    //console.log(options);
+    //console.log(Object.keys(options.cohort.coursesIndex)) //un array cuyos elementos representan las propiedaddes del objeto courseIndex
+    const courses = Object.keys(options.cohort.coursesIndex);
+    let students =computeUsersStats(options.cohortData.users,options.cohortData.progress,courses);
+  //  sortUserStats();
+  //  filterUsers();
+    return students;
+}
--- src/main.js
+++ src/main.js
@@ -1,17 +1,103 @@
-//variables de los datos que vienen del json
-const usersJson = "../data/cohorts/lim-2018-03-pre-core-pw/users.json";
+//unico url, porque contiene todos lod cohorts
 const cohortsJson = "../data/cohorts.json";
-const progressJson = "../data/cohorts/lim-2018-03-pre-core-pw/progress.json";
-//Muestra las estudiantes solo nombres
-const listStudents = document.querySelector("tbody");
+//capturar los elementos del html
+const sectionMain = document.querySelector("#space");
 const menu = document.getElementById("listCo");
+const sedeLima = document.getElementById("lim");
+const firtsMenu= document.getElementById("father");
 
-//Muestra a las alumnas
-const butOfList = document.querySelector('button');
-butOfList.addEventListener("click", leeJsons);
+//objeto options, es devuelta por la funcion processCohortData
+const options ={
+    cohort:null,
+    cohortData:{
+            users:null,
+            progress:null,
+    },
+    orderBy:"name",//mandamos valores por defecto
+    orderDirection: "ASC",
+    search:""
+};
 
-function leeJsons(){
-  new leeJson();
-          
+const getData = (str,file,callback)=>{ //callback es una funcion
+       // console.log(str,file);
+    const request = new XMLHttpRequest();
+    request.open("GET",file,true);
+    request.addEventListener("load", event =>{//current traget = xhr, target
+            if(event.target.readyState === 4){
+                    if(event.target.status !==200){
+                            return console.error(new Error(`HTTP error: ${event.target.status}`))
+                    } else{
+                            const response = JSON.parse(event.target.responseText);
+                            callback(str,response);
+                    }
+            }
+    })
+    request.send();
 }
-               
+
+
+//click al boton lima SALEN LOS COHORTS DE LIMA FILTRADOS
+const displayCohorts = (id, arrCohorts)=>{
+    const cityCohort = arrCohorts.filter(generation =>{
+        return generation.id.indexOf(id) !== -1;
+    })
+    let cohortName = "";
+   // console.log(cityCohort);
+        cityCohort.forEach(cohort =>{
+                cohortName += `
+                <option id=${cohort.id}>
+                ${cohort.id}
+                </option>
+                `;
+        });
+        menu.innerHTML = cohortName;
+}
+
+const displayProgress= (idCohort,objProgress)=>{
+        //console.log(idCohort,objProgress);
+        options.cohortData.progress = objProgress;
+        //console.log(options);
+        const studentsConStats = processCohortData(options);//objeto con propiedad
+        // let template = "";
+        // studentsConStats.forEach((objStudentConStats)=>{
+        //     template += `
+        //     ${objStudentConStats.name}
+        //     ${math.floor(objStudentconStats.stats.percent)}
+        //     ${math.floor(objStudentconStats.stats.exercises.total)}
+        //     `
+        // })
+        // sectionMain.innerHTML =template;
+    }
+
+const displayUsers= (idCohort, arrUsers)=>{
+       // console.log(idCohort,arrUsers);
+        options.cohortData.users = arrUsers; //asi metemos a la info de users en la propiedad user en option
+        getData(idCohort, `../data/cohorts/${idCohort}/progress.json`, displayProgress);
+}
+
+const cohortSelected = (idCohort, dataCohorts)=>{
+        //console.log(idCohort,dataCohorts);
+        dataCohorts.forEach(objCohort =>{
+                if(objCohort.id === idCohort){
+                        options.cohort = objCohort;
+                }
+        })
+}
+ 
+//evento de los botones q activan el select
+firtsMenu.addEventListener("click", event =>{
+    //console.log(event.target);
+    const id = event.target.id;
+    getData(id, cohortsJson, displayCohorts);
+});
+
+//evento del select q lista los cohorts
+menu.addEventListener("change", e=>{
+        let id = e.target.value;
+        //console.log( e.target.value);
+        
+        if (id != "listCo"){
+          getData(id,cohortsJson, cohortSelected);
+          getData(id,`../data/cohorts/${id}/users.json`, displayUsers);
+        }
+})
